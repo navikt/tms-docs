19865a1f6e6f0cfacd1cf01267ad990680be35de
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2026-06-15 14:04:36 +0200

    løfter prerender=false til toppen av kodefilen

--- src/pages/api/internal/isAlive.ts
+++ src/pages/api/internal/isAlive.ts
@@ -1,7 +1,7 @@
-import type { APIRoute } from "astro";
-
 export const prerender = false;
 
-export const GET: APIRoute = async function get({ params, request }) {
-  return new Response(null, { status: 200 });
+import type {APIRoute} from "astro";
+
+export const GET: APIRoute = async function get({params, request}) {
+  return new Response(null, {status: 200});
 };
--- src/pages/api/internal/isReady.ts
+++ src/pages/api/internal/isReady.ts
@@ -1,6 +1,6 @@
-import type {APIRoute} from "astro";
-
 export const prerender = false;
+
+import type {APIRoute} from "astro";
 
 export const GET: APIRoute = async function get({params, request}) {
   return new Response(null, {status: 200});
